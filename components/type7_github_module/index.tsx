1faa54b5d4ef59eb41a6f06e208839536f861f06
liangjinshuo.bytedance.com <liangjinshuo@bytedance.com>
2025-06-03 17:38:59 +0800

    [init] synchronize basic code

--- components/type7_github_module/index.tsx
+++
@@ -1,8 +0,0 @@
-// 自定义组件开发请参考：https://ae.feishu.cn/hc/zh-CN/articles/795691112919
-export interface Props {}
-
-const Type7_github_module = (props: Props): JSX.Element => {
-  return <span>Hello world!</span>;
-};
-
-export default Type7_github_module;
